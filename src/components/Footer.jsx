1810d4f2e9465fd6d40dd029a935cb037b8d7c2a
Satyam <satyamgaikwad2787@gmail.com>
2025-01-24 00:38:38 +0530

    email footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,37 +1,5 @@
-// import React from "react";
-// import { FaLinkedin, FaGithub, FaTelegram } from "react-icons/fa";
-
-// function Footer() {
-//   return (
-//     <>
-//       <hr />
-//       <footer className="py-12">
-//         <div
-//           name="Footer"
-//           className="max-w-screen-2xl container mx-auto px-4 md:px-20"
-//         >
-//           <div className="flex flex-col items-center justify-center">
-//             <div className="flex space-x-4">
-//               <FaTelegram size={24} />
-//               <FaLinkedin size={24} />
-//               <FaGithub size={24} />
-//             </div>
-//             <div className="mt-8 border-t border-grey-700 pt-8 flex flex-col items-center">
-//               <p className="text-sm">&copy; 2024 Satyam Gaikwad</p>
-//               <p className="text-sm">All rights reserved.</p>
-//             </div>
-//           </div>
-//         </div>
-//       </footer>
-//     </>
-//   );
-// }
-
-// export default Footer;
-
-
 import React from "react";
-import { FaLinkedin, FaGithub, FaTelegram } from "react-icons/fa";
+import { FaLinkedin, FaGithub, FaTelegram, FaEnvelope } from "react-icons/fa";
 
 function Footer() {
   return (
@@ -50,8 +18,14 @@ function Footer() {
               <a href="https://github.com/SatAlgo" target="_blank" rel="noopener noreferrer">
                 <FaGithub size={24} />
               </a>
-              <a href="https://t.me/yourusername" target="_blank" rel="noopener noreferrer" className="text-blue-500">
+              {/* <a href="https://t.me/yourusername" target="_blank" rel="noopener noreferrer" className="text-blue-500">
                 <FaTelegram size={24} />
+              </a> */}
+              <a
+                href="mailto:satyamgaikwad.mitaoe@gmail.com"
+                className="text-red-500"
+              >
+                <FaEnvelope size={24} />
               </a>
             </div>
             <div className="mt-8 border-t border-gray-700 pt-8 flex flex-col items-center">
